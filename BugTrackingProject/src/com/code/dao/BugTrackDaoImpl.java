d4265f1d07e7ee682b0b3d7746321e882f1db74c
abhijeet <abhijeetraut58@gmail.com>
2020-10-04 00:01:40 +0530

    Implemented ImportUsers from json file feature

--- BugTrackingProject/src/com/code/dao/BugTrackDaoImpl.java
+++ BugTrackingProject/src/com/code/dao/BugTrackDaoImpl.java
@@ -1,8 +1,57 @@
-// Author : Akanksha Shrivastava,
-// Purpose : 
+// Author : Akanksha Shrivastava, Adrija Ghansiyal
+// Purpose : Establish connection with derby db & run queries to fetch/add/modify data
 
 package com.code.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.code.bean.User;
+import com.code.dao.DBUtil;
+
 public class BugTrackDaoImpl implements BugTrackDao {
+	static Connection conn;
+	static PreparedStatement ptrial;
+	static PreparedStatement inserimportedusers;
+	
+	static {
+		conn=DBUtil.getMyConnection();
+		try {
+			//write preparedStatements here
+//			ptrial=conn.prepareStatement("");
+			inserimportedusers = conn.prepareStatement("insert into usertable values(DEFAULT,?,?,?)");
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+	// Function to save imported users into database
+	@Override
+	public int importUsers(List<User> userList) {
+		int i = 1;
+		for(User user : userList) {
+			try {
+				inserimportedusers.setString(1, user.getName());
+				inserimportedusers.setString(2, user.getEmail());
+				inserimportedusers.setString(3, user.getType());
+				
+				inserimportedusers.addBatch();
+				
+				if(i % 1000 == 0 || i == userList.size()) {
+					int[] result = inserimportedusers.executeBatch();
+					return result.length;
+				}
+				i++;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return 0;
+	}
+	
+	
 
 }
--- BugTrackingProject/src/com/code/service/BugTrackServiceImpl.java
+++ BugTrackingProject/src/com/code/service/BugTrackServiceImpl.java
@@ -3,6 +3,22 @@
 
 package com.code.service;
 
+import java.util.List;
+
+import com.code.bean.User;
+import com.code.dao.BugTrackDao;
+import com.code.dao.BugTrackDaoImpl;
+
 public class BugTrackServiceImpl implements BugTrackService{
+	BugTrackDao bugTrackDao;
+	public BugTrackServiceImpl() {
+		bugTrackDao = new BugTrackDaoImpl();
+	}
+
+	// Function to save imported users into database
+	@Override
+	public int importUsers(List<User> userList) {
+		return bugTrackDao.importUsers(userList);
+	}
 	
 }
